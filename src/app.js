79217d8753d47c6be7daffee01fbadbb232d61f6
codelikecraze <63984309+CodeLikeCrazE@users.noreply.github.com>
2022-08-25 11:07:34 -0700

    addon experiment

--- src/app.js
+++ src/app.js
@@ -1,20 +1,32 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, BrowserView } = require('electron');
 const path = require('path');
+const vm = require('vm');
+var mainWindow;
+const {VM} = require('vm2');
+
+const initializeExtensionContext = (code) => {
+  var vm = new VM();
+  vm.sandbox.openTab = (url) => {
+    mainWindow.webContents.executeJavaScript('openTab("' + url + '");');
+  }
+  vm.run(code);
+}
 
 if (require('electron-squirrel-startup')) {
   app.quit();
 }
 
 const createWindow = () => {
-  const mainWindow = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences:{ webviewTag:true },
     autoHideMenuBar: true
   });
 
-  mainWindow.loadFile(path.join(__dirname, 'index.html'));
-
+  mainWindow.loadFile(path.join(__dirname, 'index.html')).then(() => {
+    //initializeExtensionContext("for (var i = 0; i < 10; i++) { openTab('https://google.com/') }");
+  });
 };
 
 app.on('ready', createWindow);
--- src/main.js
+++ src/main.js
@@ -1,33 +1,43 @@
 //vars
 var numberOfTabs = 0;
-var currentTab = 0; 
+var currentTab = 0;
 var lastTab = 0;
 var closedTab = false;
-var webview = document.getElementById("page0") ;
+var webview = document.getElementById("page0");
+var promptList = [];
+var activePrompt = null;
+
+setInterval(function() {
+    if (!activePrompt) {
+        activePrompt = promptList.shift();
+    }
+},0);
 //navigation 
 
 document.getElementById("searchbar").addEventListener('keyup', (e) => {
-    if(e.key == "Enter") {
+    if (e.key == "Enter") {
         webview.src = "https://swisscows.com/web?query=" + document.getElementById("searchbar").value;
     }
 })
 document.getElementById("back").addEventListener('click', () => { webview.goBack(); })
 document.getElementById("front").addEventListener('click', () => { webview.goForward(); })
-document.getElementById("refresh").addEventListener('click', () => {webview.reload(); })
+document.getElementById("refresh").addEventListener('click', () => { webview.reload(); })
 
 //Tabs
 //Don't worry NineTails, your tab code was not copyed.
 webview.addEventListener("dom-ready", () => {
     document.getElementById(0).innerText = webview.getTitle();
 })
-document.getElementById("newtab").addEventListener('click', () => {
+document.getElementById("newtab").addEventListener('click',openTab);
+
+function openTab(url) {
     var newTabs = document.createElement("button");
     numberOfTabs++;
-    newTabs.setAttribute("onclick", "changeTab("+numberOfTabs+")");
+    newTabs.setAttribute("onclick", "changeTab(" + numberOfTabs + ")");
     currentTab = numberOfTabs;
 
     var newPage = document.createElement("webview");
-    newPage.src = "https://darth-ness.github.io/WarpCore-Start/";
+    newPage.src = url || "https://darth-ness.github.io/WarpCore-Start/";
     newPage.id = "page" + numberOfTabs;
     newPage.setAttribute("style", "height:88%; width:100%;display:none;");
     document.getElementById("pages").append(newPage);
@@ -40,14 +50,14 @@ document.getElementById("newtab").addEventListener('click', () => {
         newTabs.innerText = newPage.getTitle();
         document.getElementById("searchbar").value = newPage.getURL();
     })
-})
+}
 
 function changeTab(newTab) {
     //Hide old tab
     if (closedTab == false) {
         document.getElementById(lastTab).setAttribute("class", "not-selectedTAB");
         document.getElementById("page" + lastTab).setAttribute("style", "display:none;height:88%;width:100%");
-    } 
+    }
     //Show new tab
     document.getElementById(newTab).setAttribute("class", "bgBlue4");
     document.getElementById("page" + newTab).setAttribute("style", "display: default;height:88%;width:100%");
